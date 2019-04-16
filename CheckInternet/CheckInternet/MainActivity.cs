85bb2ab81f86909c4e5eb3135a463a9a28b470a0
ViktorOnegin <33310165+ViktorOnegin@users.noreply.github.com>
2019-04-16 09:20:47 +0300

    simple internet connectivity check

---
+++ CheckInternet/CheckInternet/MainActivity.cs
@@ -0,0 +1,50 @@
+﻿using Android.App;
+using System;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using Xamarin.Essentials;
+using Android.Content;
+using System.Linq;
+
+namespace CheckInternet
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.activity_main);
+
+            var profiles = Connectivity.ConnectionProfiles;
+            if (profiles.Contains(ConnectionProfile.WiFi))
+            {
+                // Active Wi-Fi connection.
+            }
+
+            // Check internet connection
+            var current = Connectivity.NetworkAccess;
+            if (current == NetworkAccess.Internet)
+            {
+                var Intent = new Intent(this, typeof(Activity1));
+                StartActivity(Intent);
+                // Connection to internet is available
+            }
+            else if(current != NetworkAccess.Internet)
+            {
+                var Intent = new Intent(this, typeof(Activity2));
+                StartActivity(Intent);
+                // Connection to internet is not available
+            }
+            Connectivity.ConnectivityChanged += Connectivity_ConnectivityChanged;
+        }
+
+        void Connectivity_ConnectivityChanged(object sender, ConnectivityChangedEventArgs args)
+        {
+            var acces = args.NetworkAccess;
+            var profiles = args.ConnectionProfiles;
+        }
+    }
+}
